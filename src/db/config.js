ef87b2d30269c53b7e5d91bcb119fa6569f9da38
LuigiBelanda <luigi.belanda.milani@gmail.com>
2021-12-26 15:20:22 -0300

    Criando o BD / Async / Await

--- src/db/config.js
+++ src/db/config.js
@@ -7,11 +7,10 @@ const { open } = require("sqlite");
 // const sqlite = require('sqlite')
 
 // Aqui colocamos alguma configs da nossa comunicação com o BD
-// o open sempre espera estar entre {}, por isso nesse caso usamos o 
+// o open sempre espera estar entre {}, por isso nesse caso usamos o
 // module.exports com uma arrow function
-module.exports = () => {
+module.exports = () =>
   open({
     filename: "./database.sqlite", // arquivo em que será guardado os dados
     driver: sqlite3.Database, // qual bd iremos usar
   });
-};
--- src/db/init.js
+++ src/db/init.js
@@ -1,75 +1,87 @@
 // Para executar esse arquivo podemos ir até a pasta que ele está e rodar o comando node init.js
 // ou podemos colocar o comando no package.json
 
-const Database = require("config");
+const Database = require("./config");
 
-// iniciando a conexão com o bd
-Database();
+const initDb = {
+  async init() {
+    // iniciando a conexão com o bd
+    const db = await Database();
+    // async / await (assíncrono)
+    // serve para colocarmos alguma espera em algumas partes do código
+    // neste caso quando o Database() terminar de executar o retorno ficará na const db
+    // o await só funciona se estiver dentro de um async, isso pq o async é responsável por mostrar qual os códigos que tem await
 
-// roda algum comando SQL
-// criando a tabela profile
-Database.exec(`CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT, 
-    avatar TEXT, 
-    monthly_budget INT,
-    days-per_week INT,
-    hours_per_week INT,
-    vacation_per_year INT,
-    value_hour INT
-)`);
+    // roda algum comando SQL
+    // criando a tabela profile
+    await db.exec(`CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT, 
+        avatar TEXT, 
+        monthly_budget INT,
+        days_per_week INT,
+        hours_per_day INT,
+        vacation_per_year INT,
+        value_hour INT
+    )`);
 
-// criando a tabela jobs
-Database.exec(`CREATE TABLE jobs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily_hours INT,
-    total_hours INT,
-    created_at DATETIME
-)`);
+    // criando a tabela jobs
+    await db.exec(`CREATE TABLE jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME
+    )`);
 
-// inserindo dados na tabela profile
-Database.run(`INSERT INTO profile (
-    name, 
-    avatar,
-    monthly-budget,
-    days_per-week,
-    hours_per_day,
-    vacation_per_year,
-) VALUES (
-    "Mayk",
-    "https://github.com/maykbrito.png",
-    3000,
-    5,
-    5,
-    4,
-    75,
-);`);
+    // inserindo dados na tabela profile
+    await db.run(`INSERT INTO profile (
+        name, 
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year,
+        value_hour
+    ) VALUES (
+        "Mayk",
+        "https://github.com/maykbrito.png",
+        3000,
+        5,
+        5,
+        4,
+        75
+    );`);
 
-// inserindo dados na tabela jobs
-Database.run(`INSERT INTO jobs (
-    name,
-    daily_hours,
-    total_hours,
-    created_at
-) VALUES (
-    "Pizzaria Guloso",
-    2,
-    1,
-    1617514376018,
-);`);
+    // inserindo dados na tabela jobs
+    await db.run(`INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "Pizzaria Guloso",
+        2,
+        1,
+        1617514376018
+    );`);
 
-Database.run(`INSERT INTO jobs (
-    name,
-    daily_hours,
-    total_hours,
-    created_at
-) VALUES (
-    "OneTwo Project",
-    3,
-    47,
-    1617514376018,
-);`);
+    await db.run(`INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+    ) VALUES (
+        "OneTwo Project",
+        3,
+        47,
+        1617514376018
+    );`);
 
-// fechando a conexão com o bd
-Database.close();
+    // fechando a conexão com o bd
+    await db.close();
+  },
+};
+
+// chamando o objeto initDb e a sua função init()
+initDb.init();
